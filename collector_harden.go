7d7a50df41b326f29a5c76a7f60ac74893b2bfbf
pos <pgousdal@users.noreply.github.com>
2026-09-04 22:09:00 +0200

    Define explicit unavailable download count error

--- collector_harden.go
+++ collector_harden.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 )
 
+var ErrDownloadCountUnavailable = errors.New("download count unavailable on GitHub package page")
+
 var downloadsPatterns = []*regexp.Regexp{
 	regexp.MustCompile(`(?is)<h3[^>]*\btitle=["']([0-9][0-9,._ ]*)["'][^>]*>.*?</h3>.{0,1000}?<span[^>]*>\s*Total downloads\s*</span>`),
 	regexp.MustCompile(`(?is)<span[^>]*>\s*Total downloads\s*</span>.{0,1000}?<h3[^>]*\btitle=["']([0-9][0-9,._ ]*)["'][^>]*>`),
@@ -23,5 +25,5 @@ func parseDownloadCount(text string) (int64, error) {
 			return v, nil
 		}
 	}
-	return 0, errors.New("download count not found on GitHub package page")
+	return 0, ErrDownloadCountUnavailable
 }
